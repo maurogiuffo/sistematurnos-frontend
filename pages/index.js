c3ae18a30f85bf33b4354e145eb1dedabaf663b9
Ezequiel <ezequielrebasa@gmail.com>
2020-05-13 23:19:31 -0300

    initial changes

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Head from 'next/head'
+import styled from '@emotion/styled';
+
+
+const Home = () => {
+  return (
+    <div>
+      <h1> Sistema de Turnos </h1>
+    </div>
+  )
+}
+
+export default Home;
